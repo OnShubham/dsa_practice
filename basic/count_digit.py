4001c017ae29088a666592159769ad087a57c65d
OnShubham <shubahmkharche3005@gmail.com>
2026-05-09 11:07:53 +0530

    cont

---
+++ basic/count_digit.py
@@ -0,0 +1,35 @@
+
+
+def count(n):
+    
+    cnt = 0
+    
+    
+    while n > 0:
+        
+        n = n // 10
+        
+        cnt += 1
+        
+    return print(cnt)
+
+n = 15
+count(n)
+
+
+
+
+def count(n):
+    
+    cnt = 0
+    
+    while n > 0:
+        
+        n = n // 10
+        
+        cnt += 1
+    
+    return print(cnt)
+
+n = 2345
+count(n)
