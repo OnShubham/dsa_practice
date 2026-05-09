d6770f5b9a5fe69c986d3476271b5f9aa0a766b0
OnShubham <shubahmkharche3005@gmail.com>
2026-05-09 12:27:26 +0530

    pushed

--- basic/practice.py
+++ basic/practice.py
@@ -23,3 +23,21 @@ def devisor(n):
 
 n = 54
 devisor(n)
+
+
+
+def reverse(n):
+    
+    rev = 0
+    
+    while n > 0:
+        ld = n % 10
+        rev = rev * 10 + ld
+        n = n // 10
+    
+    return print(rev)
+
+n = 123
+reverse(n)
+        
+        
