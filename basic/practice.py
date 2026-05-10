99725b4f08541ea3d5f45be1006a6f6b48e7fd75
OnShubham <shubahmkharche3005@gmail.com>
2026-05-10 22:30:11 +0530

    update a practice

--- basic/practice.py
+++ basic/practice.py
@@ -53,3 +53,109 @@ for char in name:
     else:
         res += char
 print(res)
+
+
+
+def reverse_nu(n):
+    
+    k = 0
+    
+    while n > 0:
+        
+        digits = n % 10
+        
+        k = k * 10 + digits
+        
+        n = n // 10
+        
+    return print(k , "s")
+
+n = 234
+reverse_nu(n)
+
+
+
+
+voevl = "voevl"
+
+name = "shuiobvel"
+
+result = ""
+
+for char in name:
+    if char in vovel:
+        result += "*"
+    else:
+        result += char
+print(result)
+
+
+
+def find_repeatetd_word(n):
+    
+    dict = {}
+    
+    for char in n:
+        
+        if char in dict:
+            dict[char] += 1
+        else:
+            dict[char] = 1
+    for char, count in dict.items():
+        print(char, count)
+            
+        
+        
+    
+
+
+
+
+
+
+n = "shubamssdsh"
+find_repeatetd_word(n)
+
+
+
+def find_char_count(text):
+    
+    char_count = {}
+    
+    for char in text:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+            
+    for char, count in char_count.items():
+        print(f"{char} -> {count}")
+    # return print(char_count)
+
+text = "asdfghjkasdfghjksdfghj"
+find_char_count(text)
+
+
+
+def divisor_num(n):
+    
+    if n == 0:
+        return
+    
+    res = []
+    
+    for i in range(1, n + 1):
+        
+        if n % i == 0:
+            res.append(i)
+            
+    print(res)
+
+n = 12
+    
+divisor_num(n)
+    
+        
+        
+        
+        
